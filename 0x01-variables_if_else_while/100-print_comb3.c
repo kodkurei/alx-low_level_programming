51e1af922cc70a3fccbd50dcd3b8e935b757cfe8
kodkurei <kureiallan47@gmail.com>
2023-02-18 01:13:42 +0300

    ready

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,10 +17,10 @@ int main(void)
 	if (m > n)
 	{
 																						putchar(n);																				putchar(m);
-	if (n !=56 || m != 57)
+	if (n != 56 || m != 57)
 																						{																																										putchar(',');																				putchar(' ');
 																						}
-																						}
+	}
 	}
 	}
 	putchar('\n');
