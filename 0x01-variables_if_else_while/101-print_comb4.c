47904db9fdda468ac9d12ff39ddad808c1546849
kodkurei <kureiallan47@gmail.com>
2023-02-18 00:28:03 +0300

    updates

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,16 +20,16 @@
 
 	for (q = p + 1; q <= '9'; q++)
 	{
-		if ((p !=d) != q)
-		{
-			putchar(d);
-			putchar(p);
-			putchar(q);
-			if (d == '7' && p == '8')
-			continue;
-			putchar(',');
-			putchar(' ');
-		}
+	if ((p !=d) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	}
 	}
