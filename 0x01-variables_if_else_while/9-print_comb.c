5607d752b07a5ed336f40ed099e929f5eb7d6255
kodkurei <kureiallan47@gmail.com>
2023-02-17 21:21:33 +0300

    9-print_comb.c updates

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,7 @@ int main(void)
 {
 
 	int d;
-
+	
 	for (d = '0'; d <= '9'; d++)
 	{
 	putchar(d);
@@ -22,6 +22,6 @@ int main(void)
 	putchar(' ');
 	}
 	}
-      	putchar ('\n');
+      	putchar('\n');
 	return (0);
 }
