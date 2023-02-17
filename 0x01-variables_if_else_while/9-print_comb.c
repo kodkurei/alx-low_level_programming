5f58c7be95c499efb88c485664b6039a3afa733d
kodkurei <kureiallan47@gmail.com>
2023-02-18 01:20:52 +0300

    add

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -9,6 +8,7 @@
 int main(void)
 
 {
+
 	int d;
 	
 	for (d = '0'; d <= '9'; d++)
