9b39638ab24cedbf65c9e47b3ded799373529d54
kodkurei <kureiallan47@gmail.com>
2023-02-18 23:26:59 +0300

    add

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error.
- * Return: Always 1.
- */
-
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+*/
 
 int main(void)
 
