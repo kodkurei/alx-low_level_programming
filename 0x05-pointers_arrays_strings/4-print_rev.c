353d9d74986a9d0d4107853715879e143ef67ce8
kodkurei <kureiallan47@gmail.com>
2023-02-27 22:03:05 +0300

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,6 @@ void print_rev(char *s)
 {
 	int longi = 0;
 	int o;
-	
 	while (*s != '\0')
 	{
 		longi++;
@@ -19,7 +18,6 @@ void print_rev(char *s)
 	{
 		_putchar(*s);
 		s--;
-	}
-	
+	}	
 	_putchar('\n');
 }
