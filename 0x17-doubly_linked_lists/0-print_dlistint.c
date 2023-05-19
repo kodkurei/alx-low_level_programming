9512fdd012fa71bbb7e034c25648fb1896666fac
kodkurei <kureiallan47@gmail.com>
2023-05-19 21:43:54 +0300

    to date

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,9 @@
 #include"lists.h"
 /**
- * print_dlistint-Function that prints all the elements of a dlistint_t
- * @h: Pointer to the head node of the list
- * Return: the number of nodes in the list
- */
+ *  *print_dlistint-Function that prints all the elements of a dlistint_t
+ *   *@h: Pointer to the head node of the list
+ *    *Return: the number of nodes in the list
+ *     */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;/* pointer to the current node */
